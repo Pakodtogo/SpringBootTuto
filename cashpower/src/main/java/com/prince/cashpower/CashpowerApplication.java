0f438928ace1c47c2b5f34320a81d04d9c240513
Pakodtogo <pakodtogoj@gmail.com>
2024-01-24 17:10:13 +0000

    ça va aller

---
+++ cashpower/src/main/java/com/prince/cashpower/CashpowerApplication.java
@@ -0,0 +1,13 @@
+package com.prince.cashpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashpowerApplication.class, args);
+	}
+
+}
