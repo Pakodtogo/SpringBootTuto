6289b60980bc09b9d9ab8780fe7e5715a17f718f
Pakodtogo <pakodtogoj@gmail.com>
2024-01-27 09:35:39 +0000

    'bon'

--- cashpowerSonabel/cashpower/admin.py
+++ cashpowerSonabel/cashpower/admin.py
@@ -1,15 +1,59 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
 from .models import Abonnement, Exploitation, PointDeVente, Guichet, Branchement, EtatAbonnement, Compteur, Abonne, Tarif, TrancheTarif, Vente
 
 # Register your models here.
-admin.site.register(Branchement)
-admin.site.register(Exploitation)
-admin.site.register(PointDeVente)
-admin.site.register(Guichet)
-admin.site.register(EtatAbonnement)
-admin.site.register(Compteur)
-admin.site.register(Abonne)
-admin.site.register(Tarif)
-admin.site.register(TrancheTarif)
-admin.site.register(Abonnement)
-admin.site.register(Vente)
+class AdminAbonne(admin.ModelAdmin):
+    list_display = ('id', 'nom', 'prenom', 'numero_telephone', 'adresse_mail', 'numero_cnib')
+
+class AdminAbonnement(admin.ModelAdmin):
+    list_display = ('id', 'numero_abonnement', 'date_creation', 'date_mise_en_service', 'date_resiliation', 'timbre', 'liasse', 'numero_police', 'frais_police',)
+
+class AdminBranchement(admin.ModelAdmin):
+    list_display = ('id', 'type_branchement', 'section', 'lot', 'parcelle', 'rang', 'exploitation')
+    
+class AdminCompteure(admin.ModelAdmin):
+    list_display = ('numero_compteur', 'modele_compteur', 'mode_facturation', 'numero_carte')
+    class Media:
+        css = {
+            'all': ('css/custom_admin_styles.css',),
+        }
+
+class AdminEtatAbonnement(admin.ModelAdmin):
+    list_display = ('code_etat', 'libele')
+    
+class AdminExploitation(admin.ModelAdmin):
+    list_display = ('numero_exploitation', 'designation')
+    
+class AdminPointDeVente(admin.ModelAdmin):
+    list_display = ('code_pdv', 'libelle', 'exploitation')
+ 
+class AdminGuichet(admin.ModelAdmin):
+    list_display = ('numero_guichet', 'pointDeVente')
+
+class AdminTarif(admin.ModelAdmin):
+    list_display = ('code_tarif', 'prime_fixe', 'redevance', 'amperage')
+     
+class AdminTrancheTarif(admin.ModelAdmin):
+    list_display = ('id', 'debut', 'fin', 'cout_ht', 'tde_trch', 'tsdaae_trch', 'tva_trch', 'tarif')
+   
+class AdminVente(admin.ModelAdmin):
+    list_display = ('numero_ticket', 'montant', 'tqr', 'date', 'energie_paye', 'tde', 'tsdaae', 'tva', 'cout_energie', 'code_token', 'abonnement', 'guichet')
+       
+admin.site.register(Branchement, AdminBranchement)
+admin.site.register(Exploitation, AdminExploitation)
+admin.site.register(PointDeVente, AdminPointDeVente)
+admin.site.register(Guichet, AdminGuichet)
+admin.site.register(EtatAbonnement, AdminEtatAbonnement)
+admin.site.register(Compteur, AdminCompteure)
+admin.site.register(Abonne, AdminAbonne)
+admin.site.register(Tarif, AdminTarif)
+admin.site.register(TrancheTarif, AdminTrancheTarif)
+admin.site.register(Abonnement, AdminAbonnement)
+admin.site.register(Vente, AdminVente)
+
+# Configuration du titre, de l'en-tête et du titre de l'index
+admin.site.site_title = _("CASHPOWER SYSTEM")
+admin.site.site_header = _("CASHPOWER SYSTEM")
+admin.site.index_title = _("CASHPOWER SYSTEM")
+
--- cashpowerSonabel/cashpower/views.py
+++ cashpowerSonabel/cashpower/views.py
@@ -1,4 +1,5 @@
 # gestion_abonnements/views.py
+import random
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Abonne, Abonnement, Vente, Branchement
 from .forms import AbonneForm, AbonnementForm, VenteForm
@@ -46,34 +47,28 @@ def abonnement_list(request):
 
 def abonnement_detail(request, pk):
     abonnement = get_object_or_404(Abonnement, pk=pk)
-    return render(request, 'abonnement_detail.html', {'abonnement': abonnement})
+    return render(request, 'abonnements/detail.html', {'abonnement': abonnement})
 
 def abonnement_create(request):
     if request.method == 'POST':
         form = AbonnementForm(request.POST)
-        branchement_temp = form.fields['branchement']
-        
-        abonnemnent_save = Abonnement ()
-        # abonnemnent_save.abonne = form.fields['abonne']
-        # abonnemnent_save.branchement = form.fields['branchement']
-        # abonnemnent_save.etat_abonnement = form.fields['etat_abonnement']
-        abonnemnent_save.compteur = form.fields['compteur']
-        abonnemnent_save.tarif = form.fields['tarif']
-        abonnemnent_save.guichet = form.fields['guichet']
-        
-        abonnemnent_save.numero_abonnement = f"{branchement_temp.section} {branchement_temp.lot} {branchement_temp.parcelle} {branchement_temp.rang}"
-        
-        abonnemnent_save.timbre = 500
-        abonnemnent_save.liasse = 1500
-        abonnemnent_save.frais_police = 2000
-        abonnemnent_save.numero_police = ''.join([str(int(char)) for char in abonnemnent_save.numero_abonnement.replace(' ', '')])
-        
-        abonnemnent_save.save()
-        return redirect('abonnement_list')
-        
+        if form.is_valid():
+            abonnement = form.save(commit=False)
+            
+            branchement_temp = abonnement.branchement
+            abonnement.numero_abonnement = f"{branchement_temp.section} {branchement_temp.lot} {branchement_temp.parcelle} {branchement_temp.rang}"
+            abonnement.timbre = 500
+            abonnement.liasse = 1500
+            abonnement.frais_police = 2000
+            abonnement.numero_police = ''.join([str(int(char)) for char in abonnement.numero_abonnement.replace(' ', '')])
+            
+            abonnement.save()
+
+            return redirect('abonnement_list')        
     else:
         form = AbonnementForm()
     return render(request, 'abonnements/create.html', {'form': form})
+
 
 def abonnement_edit(request, pk):
     abonnement = get_object_or_404(Abonnement, pk=pk)
@@ -92,21 +87,56 @@ def abonnement_edit(request, pk):
 
 def vente_list(request):
     ventes = Vente.objects.all()
-    return render(request, 'vente_list.html', {'ventes': ventes})
+    return render(request, 'ventes/index.html', {'ventes': ventes})
 
 def vente_detail(request, pk):
     vente = get_object_or_404(Vente, pk=pk)
     return render(request, 'vente_detail.html', {'vente': vente})
 
 def vente_create(request):
+    
+    def calcul_TQR(M):
+        if M < 100:
+            TQR = 0
+        elif 100 <= M <= 1000:
+            TQR = 20
+        elif 1000 < M <= 5000:
+            TQR = 30
+        elif 5000 < M <= 10000:
+            TQR = 50
+        elif 10000 < M <= 50000:
+            TQR = 100
+        else:
+            n = M // 50000
+            TQR = 50 * (n + 2)
+        return TQR
+    
+
     if request.method == 'POST':
         form = VenteForm(request.POST)
         if form.is_valid():
-            vente = form.save()
-            return redirect('vente_detail', pk=vente.pk)
+            vente = form.save(commit=False)
+            vente.tqr = calcul_TQR(vente.montant)
+            p = vente.abonnement.tarif.prime_fixe
+            r = vente.abonnement.tarif.redevance
+            mat = vente.montant - p - r - vente.tqr
+            eht = mat/108
+            vente.tde = 2 * eht
+            vente.tsdaae = 3*eht
+            vente.tva = 18 * eht / 100
+            vente.energie_paye = eht - vente.tde - vente.tsdaae - vente.tva
+            vente.cout_energie = vente.energie_paye * 108
+            groups_of_digits = [str(random.randint(0, 9999)).zfill(4) for _ in range(5)]
+            vente.code_token = ' '.join(groups_of_digits)
+            vente.numero_ticket = groups_of_digits
+            
+            vente.save()
+
+            return redirect('vente_list') 
+            
     else:
         form = VenteForm()
-    return render(request, 'vente_form.html', {'form': form})
+    return render(request, 'ventes/create.html', {'form': form})
 
 def vente_edit(request, pk):
     vente = get_object_or_404(Vente, pk=pk)
